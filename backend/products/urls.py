170dcf7aa02ce58a5be1e9b00c671413851cf37b
unknown <adegbiteismail27@gmail.com>
2022-09-08 23:04:48 -0700

    updates

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ProductDetailAPIView, ProductListCreateAPIView
+from .views import ProductDetailAPIView, ProductListCreateAPIView,\
+    ProductUpdateAPIView, ProductDeleteAPIView, ProductMixins,\
+    SingleProductMixin, RetrieveUpdateDelete
 
 urlpatterns = [
-    path('', ProductListCreateAPIView.as_view()),
-    path('<int:pk>/', ProductDetailAPIView.as_view())
+    path('', ProductMixins.as_view()),
+    # path('<int:pk>/', SingleProductMixin.as_view()),
+    path('<int:pk>/', RetrieveUpdateDelete.as_view()),
+    path('<int:pk>/update/', ProductUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', ProductDeleteAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,8 @@
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import generics
+from rest_framework.mixins import ListModelMixin, CreateModelMixin,\
+     UpdateModelMixin, RetrieveModelMixin, DestroyModelMixin
 
 # Create your views here.
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -13,10 +15,93 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if not content:
             content = title
         serializer.save(content=content)
-        return super().perform_create(serializer)
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            instance.save()
+
+class ProductDeleteAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+
+
+class ProductMixins(
+    ListModelMixin,
+    CreateModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args,**kwargs):
+        return self.list(request, *args,**kwargs)
+
+    def post(self, request, *args,**kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if not content:
+            content = title
+        serializer.save(content=content)
+
+class SingleProductMixin(RetrieveModelMixin,
+                        UpdateModelMixin,
+                        DestroyModelMixin,
+                        generics.GenericAPIView
+                    ):
+    queryset = Product.objects.all()
+    lookup_field = 'pk'
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            instance.save()
+    
+    def perform_destroy(self, instance):
+        print('deleted')
+        print(instance.title)
+
+class RetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            instance.save()
+
+    def perform_destroy(self, instance):
+        print(f'{instance.title} deleted')
+        return super().perform_destroy(instance)
 
 
